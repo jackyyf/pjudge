587c7df1a9a392d1833ad4cbd210bebeffd3b560
Jack YYF <root@jackyyf.com>
2012-09-01 21:17:01 +0800

    'Add Spj demo'

---
+++ src/demo_spj.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import judge
+
+status = {-1: 'Compile Error', 0: 'Accepted', 1: 'Wrong Answer', 2: 'Runtime Error', 3: 'CPU Limit Exceeded', 4: 'Memory Limit Exceeded', 5: 'Idleness Limit Exceeded', -2147483648: 'Contact Staff', -2: 'Hacking Attempt Detected', 6: 'Partial Points'}
+
+def my_compare(usr, std):
+	usr = float(usr)
+	std = float(std)
+	return usr / std
+
+_judge = judge.Judge(3.0, 16777216)
+print 'Verdict:', status[_judge.judge("C++", "random.cpp", "random.in", "random.out", compare = my_compare)]
+print 'CPU Usage:', _judge.cpuusage.value, 's'
+print 'Memory Usage: %.2f KB' % (float(_judge.memoryusage.value * 100 / 1024) / 100)
+print 'Score Ratio:', _judge.score.value
